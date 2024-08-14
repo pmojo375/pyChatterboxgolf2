d531d03b5aa4ee6959a9959774e11db4adcd7a2e
Parker Mojsiejenko <pmojo375@gmail.com>
2024-08-14 14:34:06 -0400

    chore: Remove unnecessary files and code

--- cbg/main/management/commands/verify_timezone_aware.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from main.models import Week
-
-class Command(BaseCommand):
-    help = 'Verify timezone awareness of Week dates'
-
-    def handle(self, *args, **kwargs):
-        for week in Week.objects.all():
-            self.stdout.write(f'Week {week.id}: {week.date} (tzinfo: {week.date.tzinfo})')
