ff422a8525f67b1edb2aaa84b3a7866e88d5d6c6
Parker Mojsiejenko <pmojo375@gmail.com>
2025-07-27 21:33:43 -0400

    Refactor settings.py to enhance security and database configuration
    - Replaced hardcoded SECRET_KEY with a secure retrieval method using decouple.
    - Updated DATABASES configuration to switch from SQLite to PostgreSQL, including user credentials and connection details.

--- cbg/cbg/settings.py
+++ cbg/cbg/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-72pt+s+eb8y_ou^c%(!xph7!yo=c%df1ajs)t&9-bm=s+9*&7$'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,8 +80,12 @@ WSGI_APPLICATION = 'cbg.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cbg',
+        'USER': 'pmojo375',
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
