8f0dc5a48d855393fd9243ae52388f9acae2f5b1
Parker Mojsiejenko <pmojo375@gmail.com>
2025-07-27 21:45:36 -0400

    Update ALLOWED_HOSTS in settings.py to include localhost for local development

--- cbg/cbg/settings.py
+++ cbg/cbg/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['chatterboxgolf.com', 'www.chatterboxgolf.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'chatterboxgolf.com', 'www.chatterboxgolf.com']
 
 # Application definition
 
